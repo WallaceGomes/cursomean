81d2d32cc365e55a22ee9e60a97ab35043c6d36a
unknown <wallacecardosogomes@gmail.com>
2019-03-22 22:02:49 -0300

    Components, listening, output, angular material

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'No Content';
 
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
 }
