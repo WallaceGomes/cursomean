8e58b0687d3942a80ba23eeed40d87db7ba891bc
unknown <wallacecardosogomes@gmail.com>
2019-04-09 22:37:35 -0300

    Aplicando nodeJS ao projeto

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+   "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+  "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+app.post("/api/posts",(req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added succesfully'
+  })
+});
+
+app.get("/api/posts" ,(req, res, next) => {
+  const posts = [
+    {
+      id: "fasdasdas",
+      titlle: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "fasdasdas",
+      titlle: "Second server-side post",
+      content: "This is coming from the server"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succersfully"',
+    posts: posts
+  });
+});
+
+module.exports = app;
